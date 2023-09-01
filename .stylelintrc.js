8fe3bd5e98aa50ea9e74362e14f6997f61f87b48
obukata <obukata@mj-star.jp>
2023-09-01 11:37:35 +0900

    stylelint ルール更新

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,4 +5,17 @@ module.exports = {
     '**/lib/*.css',
     '**/noe_modules/**',
   ],
+  "rules": {
+    "selector-id-pattern": null,
+    "selector-class-pattern": null,
+    "keyframes-name-pattern": null,
+    "function-url-quotes": ["always", { "severity": "warning" }],
+    "number-max-precision": [3, { "severity": "warning" }],
+    "alpha-value-notation": ["number", { "severity": "warning" }],
+    "font-family-name-quotes": ["always-where-recommended", { "severity": "warning" }],
+    "property-no-vendor-prefix": [true, { "ignoreProperties": ["appearance", "text-size-adjust"] }],
+    "no-descending-specificity": null,
+    "no-duplicate-selectors": null,
+    "declaration-block-no-duplicate-custom-properties": null,
+  }
 }
